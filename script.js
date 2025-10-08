81e254a6547798191cb2195e2371e1316b29773d
Andrujus <justasandrusk@gmail.com>
2025-10-08 08:27:32 +0300

    javascript funkcionalumas

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById("7").addEventListener("click", function() {
+    document.getElementById("display").value += "7";
+});
+document.getElementById("8").addEventListener("click", function() {
+    document.getElementById("display").value += "8";
+}
+);
+document.getElementById("9").addEventListener("click", function() {
+    document.getElementById("display").value += "9";
+});
+document.getElementById("4").addEventListener("click", function() {
+    document.getElementById("display").value += "4";
+});
+document.getElementById("5").addEventListener("click", function() {
+    document.getElementById("display").value += "5";
+});
+document.getElementById("6").addEventListener("click", function() {
+    document.getElementById("display").value += "6";
+});
+document.getElementById("1").addEventListener("click", function() {
+    document.getElementById("display").value += "1";
+});
+document.getElementById("2").addEventListener("click", function() {
+    document.getElementById("display").value += "2";
+});
+document.getElementById("3").addEventListener("click", function() {
+    document.getElementById("display").value += "3";
+});
+document.getElementById("0").addEventListener("click", function() {
+    document.getElementById("display").value += "0";
+});
+document.getElementById("/").addEventListener("click", function() {
+    document.getElementById("display").value += "/";
+});
+document.getElementById("*").addEventListener("click", function() {
+    document.getElementById("display").value += "*";
+});
+document.getElementById("-").addEventListener("click", function() {
+    document.getElementById("display").value += "-";
+});
+document.getElementById("+").addEventListener("click", function() {
+    document.getElementById("display").value += "+";
+});
+document.getElementById(".").addEventListener("click", function() {
+    document.getElementById("display").value += ".";
+});
+document.getElementById("C").addEventListener("click", function() {
+    document.getElementById("display").value = "";
+});
