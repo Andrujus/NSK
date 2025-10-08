6fe1a8a0e6707edcd50c26cb0a96b9c3dac93239
Andrujus <justasandrusk@gmail.com>
2025-10-08 08:42:10 +0300

    eval

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 document.getElementById("7").addEventListener("click", function() {
     document.getElementById("display").value += "7";
+    a = 7;
 });
 document.getElementById("8").addEventListener("click", function() {
     document.getElementById("display").value += "8";
+    a = 8;
 }
 );
 document.getElementById("9").addEventListener("click", function() {
     document.getElementById("display").value += "9";
+    a = 9;
 });
 document.getElementById("4").addEventListener("click", function() {
     document.getElementById("display").value += "4";
+    a = 4;
 });
 document.getElementById("5").addEventListener("click", function() {
     document.getElementById("display").value += "5";
+    a = 5;
 });
 document.getElementById("6").addEventListener("click", function() {
     document.getElementById("display").value += "6";
+    a = 6;
 });
 document.getElementById("1").addEventListener("click", function() {
     document.getElementById("display").value += "1";
@@ -47,3 +53,8 @@ document.getElementById(".").addEventListener("click", function() {
 document.getElementById("C").addEventListener("click", function() {
     document.getElementById("display").value = "";
 });
+document.getElementById("=").addEventListener("click", calculate);
+
+function sum() {
+        document.getElementById("display").value 
+};
